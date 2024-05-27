5d2f7031466732f153c08e20a953e24fb0151b75
heejjinkim <06.hjhj.12@gmail.com>
2024-05-27 15:13:36 +0900

    controller와 service 추가

---
+++ src/main/java/umc/spring/service/TempService/TempCommandServiceImpl.java
@@ -0,0 +1,8 @@
+package umc.spring.service.TempService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TempCommandServiceImpl {
+}
