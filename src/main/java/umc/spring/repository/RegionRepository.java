5e45d8a981f6a7cdb09c39cb94e154cf34699e01
heejjinkim <06.hjhj.12@gmail.com>
2024-06-26 22:31:07 +0900

    :sparkles: repository 추가

---
+++ src/main/java/umc/spring/repository/RegionRepository.java
@@ -0,0 +1,6 @@
+package umc.spring.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import umc.spring.domain.Region;
+
+public interface RegionRepository extends JpaRepository<Region, Long> {
+}
