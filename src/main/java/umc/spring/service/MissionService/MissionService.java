a6e6ad3459e43edcd191618ab0a151d53db6de64
heejjinkim <06.hjhj.12@gmail.com>
2024-06-27 03:09:37 +0900

    :sparkles: feat: service 추가 #5

--- src/main/java/umc/spring/service/MissionService/MissionService.java
+++ src/main/java/umc/spring/service/MissionService/MissionService.java
@@ -1,7 +1,13 @@
 package umc.spring.service.MissionService;
 import umc.spring.domain.Mission;
+import umc.spring.domain.mapping.MemberMission;
 import umc.spring.web.dto.MissionRequestDTO;
 
 public interface MissionService {
     public Mission createMission(MissionRequestDTO.CreateDTO request);
+    public MemberMission challengeMission(MissionRequestDTO.ChallengeDTO request);
+
+    boolean existMission(Long value);
+
+    boolean isChallengingMission(Long memberId, Long missionId);
 }
--- src/main/java/umc/spring/service/MissionService/MissionServiceImpl.java
+++ src/main/java/umc/spring/service/MissionService/MissionServiceImpl.java
@@ -5,6 +5,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import umc.spring.converter.MissionConverter;
 import umc.spring.domain.Mission;
+import umc.spring.domain.enums.MissionStatus;
+import umc.spring.domain.mapping.MemberMission;
+import umc.spring.repository.MemberMissionRepository;
+import umc.spring.repository.MemberRepository;
 import umc.spring.repository.MissionRepository;
 import umc.spring.repository.StoreRepository;
 import umc.spring.web.dto.MissionRequestDTO;
@@ -16,6 +20,8 @@ public class MissionServiceImpl implements MissionService{
 
     private final MissionRepository missionRepository;
     private final StoreRepository storeRepository;
+    private final MemberMissionRepository memberMissionRepository;
+    private final MemberRepository memberRepository;
 
     @Override
     @Transactional
@@ -24,5 +30,27 @@ public class MissionServiceImpl implements MissionService{
         mission.setStore(storeRepository.findById(request.getStoreId()).get());
 
         return missionRepository.save(mission);
+    }
+
+    @Override
+    @Transactional
+    public MemberMission challengeMission(MissionRequestDTO.ChallengeDTO request) {
+        MemberMission memberMission = MissionConverter.toMemberMission();
+        memberMission.setMember(memberRepository.findById(request.getMemberId()).get());
+        memberMission.setMission(missionRepository.findById(request.getMissionId()).get());
+
+        return memberMissionRepository.save(memberMission);
+    }
+
+    @Override
+    public boolean existMission(Long value) {
+        return missionRepository.existsById(value);
+    }
+
+    @Override
+    public boolean isChallengingMission(Long memberId, Long missionId) {
+        return memberMissionRepository.findByMissionIdAndMemberId(missionId, memberId)
+                .map(memberMission -> memberMission.getStatus().name().equals("CHALLENGING"))
+                .orElse(false);
     }
 }
